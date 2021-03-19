9c31d3f69f5e90cd11c39c64220ebfa3bc4f5932
Paul Garrod <paulrgarrod@gmail.com>
2021-03-20 12:44:21 +1300

    implement localStorage CRUD
    integrated localStorage. Updated CRUD functionality with localStorage

--- assets/JS/toDo.js
+++ assets/JS/toDo.js
@@ -1,46 +1,57 @@
-let todos = ["Finish Portfolio",
-"Take Lisa for Sushi",
-"Apply for Dev Jobs",
-"take dog for a walk"
-];
+let todosArray = localStorage.getItem('items')
+  ? JSON.parse(localStorage.getItem('items'))
+  : []
 
-//function to create li's from the array
-todos.forEach(function(todo){
-    console.log(`${todo}`);
+const newTodo = (todo) => {
     $("ul").append(`
     <li>
-    <span class='trashIcon'><i class='fa fa-trash'></i>
     </span><span class='todoText'>${todo}</span>
     <span class='flagIcon'><i class='far fa-flag'></i></span>
-    </li>`);
+    <span class='trashIcon'><i class='fa fa-trash'></i>
+    </li>
+    `);
+}
+
+//function to create li's from the array
+todosArray.forEach((todo) => {
+    newTodo(todo);
 });
 
 $("input[type='text']").keypress(function(e){
     if(e.which === 13){
-        var todoText = $(this).val();
-        $(this).val("");
-        //change this to add todo to array
-        $("ul").append(`
-        <li>
-        <span class='trashIcon'><i class='fa fa-trash'></i>
-        </span><span class='todoText'>${todoText}</span>
-        <span class='flagIcon'><i class='far fa-flag'></i></span>
-        </li>`);
-        };
+        newTodo(this.value);
+        todosArray.push(this.value)
+        console.log(todosArray)
+        localStorage.setItem('items', JSON.stringify(todosArray))
+        this.value = '';
+    };
+});
+
+const findIndex = (todo) => {
+    const index = todosArray.findIndex(x => x === todo);
+    console.log(index);
+    return index;
+}
+
+const deleteTodo = (i) => {
+    const index = findIndex(i)
+    todosArray.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(todosArray))
+};
+
+//click on x to delete todo
+$("ul").on("click", ".trashIcon", function(e){
+    deleteTodo(e.target.parentNode.parentNode.innerText)
+    localStorage.setItem('items', JSON.stringify(todosArray))
+    $(this).parent().fadeOut(750, function(){
+        $(this).remove();
+    });
+    console.log(todosArray)
 });
 
 //check off specific todos by clicking
 $("ul").on("click", ".todoText", function(){
     $(this).toggleClass("completed");
-});
-
-//click on x to delete todo
-$("ul").on("click", ".trashIcon", function(e){
-    $(this).parent().fadeOut(750, function(){
-        $(this).remove();
-    });
-    e.stopPropagation();
-    //this stops event bubbling up and triggering parent events
 });
 
 $("ul").on("click", ".flagIcon", function () {
